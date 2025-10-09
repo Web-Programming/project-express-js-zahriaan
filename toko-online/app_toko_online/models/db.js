886b8e2a683ed17b2ee3503658036ca6f8cdc9d9
zahriaan <abelmuhammadzahrian_2327240042@mhs.mdp.ac.id>
2025-10-09 19:33:31 +0700

    Pertemuan8

---
+++ toko-online/app_toko_online/models/db.js
@@ -0,0 +1,16 @@
+const mongoose = require("mongoose");
+const dbURI = "mongodb://localhost:27017/paw2-si5c";
+
+mongoose.connect(dbURI);
+
+mongoose.connection.on("connected", () => {
+  console.log(`Mongoose connected to ${dbURI}`);
+});
+
+mongoose.connection.on("error", (err) => {
+  console.error("Mongoose connection error:", err);
+});
+
+mongoose.connection.on("disconnected", () => {
+  console.log("Mongoose disconnected");
+});
